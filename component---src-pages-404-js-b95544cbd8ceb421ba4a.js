a15c5164d32116efe9a2a7ced84cb26d7dba968a
Louise Davies <louise.davies@stfc.ac.uk>
2019-01-30 14:15:57 +0000

    Updates

---
+++ component---src-pages-404-js-b95544cbd8ceb421ba4a.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{149:function(t,e,n){"use strict";n.r(e);var u=n(7),o=(n(0),n(153)),s=n(152);e.default=function(){return Object(u.b)(o.a,null,Object(u.b)(s.a,{title:"404: Not found"}),Object(u.b)("h1",null,"NOT FOUND"),Object(u.b)("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-b95544cbd8ceb421ba4a.js.map
